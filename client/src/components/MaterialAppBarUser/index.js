2b5899c336d3a62034958999c86869bf3845b1bb
Brett B <65505937+BreadBooks@users.noreply.github.com>
2021-01-13 21:10:52 -0600

    Fixed SignIn route to userDashboard

--- client/src/components/MaterialAppBarUser/index.js
+++ client/src/components/MaterialAppBarUser/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import userAPI from "../../utils/userAPI"
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
@@ -7,7 +7,9 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-
+import Drawer from '@material-ui/core/Drawer'
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
 const AppBarU = AppBar
 
@@ -23,33 +25,46 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-export default function MaterialAppBarU () {
+
+export default function MaterialAppBarU() {
+    const [open, setOpen] = useState(false)
+
+    const handleDrawer = () => {
+        setOpen(true)
+    }
+
     const classes = useStyles();
-  
+
     function logoutEvent(event) {
         event.preventDefault();
-            userAPI.logoutUser().then(() => {
-              window.location.replace("/");
-            })
-                .catch(err => console.log(err));
-      };
-  
-    
+        userAPI.logoutUser().then(() => {
+            window.location.replace("/");
+        })
+            .catch(err => console.log(err));
+    };
+
+
     return (
         <div className={classes.root}>
             <AppBarU position="absolute">
-                <div>
                     <Toolbar>
-                        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={handleDrawer}>
                             <MenuIcon />
                         </IconButton>
                         <Typography variant="h6" className={classes.title}>
                             Formulater
                         </Typography>
-                    <Button color="inherit" onClick={logoutEvent}>Logout</Button>
-                </Toolbar>
-                </div>
-                </AppBarU>
+                        <Button color="inherit" onClick={logoutEvent}>Logout</Button>
+                    </Toolbar>
+            </AppBarU>
+
+            <Drawer 
+                anchor='left'
+                open={open}
+                onClose={!open}
+            >
+                <h1>User documents</h1>
+                </Drawer>
         </div>
     )
 }
--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -75,7 +75,7 @@ export default function SignInSide() {
         username: "",
         password: ""
       })).then((req) => {
-        window.location.replace("/dashboard");
+        window.location.replace("/userDashboard");
       })
         .catch(err => {
           console.log(err)
