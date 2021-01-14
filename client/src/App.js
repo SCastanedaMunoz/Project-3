b3ee3b3b33e9266131eb79eb878123faea567e98
James Howard <jth2698@gmail.com>
2021-01-13 21:18:35 -0600

    add userData object inside api call to user data

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Dashboard from "./pages/dashboard"
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-import TestPage from "./pages/TestPage";
 
 function App() {
 
@@ -14,7 +13,6 @@ function App() {
           <Route path="/dashboard" exact component={Dashboard}><Dashboard /></Route>
           <Route path="/" exact component={SignIn}><SignIn /></Route>
           <Route path="/SignUp" exact component={SignUp}><SignUp /></Route>
-          <Route path="/test" exact component={TestPage}></Route>
         </Switch>
       </div>
     </Router>
